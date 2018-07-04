bb12d3dfd6595463cf4139e552e09ece779c09e3
Nida-Krailas <nkrailas@gmail.com>
2018-07-04 02:42:24 -0500

    Adding tabs - in progress

--- app/src/main/java/com/example/android/miwok/FixedTabsPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/FixedTabsPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,8 +10,11 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class FixedTabsPagerAdapter extends FragmentPagerAdapter {
 
-    public FixedTabsPagerAdapter(FragmentManager fragmentManager) {
+    private Context context;
+
+    FixedTabsPagerAdapter(FragmentManager fragmentManager, Context context) {
         super(fragmentManager);
+        this.context = context;
     }
 
     @Override
@@ -24,6 +28,21 @@ public class FixedTabsPagerAdapter extends FragmentPagerAdapter {
         } else {
             return new PhrasesFragment();
         }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return context.getString(R.string.numbers_title);
+        } else if (position == 1) {
+            return context.getString(R.string.family_title);
+        } else if (position == 2) {
+            return context.getString(R.string.colors_title);
+        } else if (position == 3) {
+            return context.getString(R.string.phrases_title);
+        }
+
+        return null;
     }
 
     @Override
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,13 +15,11 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        PagerAdapter pagerAdapter = new FixedTabsPagerAdapter(getSupportFragmentManager());
+        PagerAdapter pagerAdapter = new FixedTabsPagerAdapter(getSupportFragmentManager(), this);
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(pagerAdapter);
